6d9b8ad9f0ef616e4b5d83cb30c9511b1e75f616
MAJESTY <ugochukwuaugustine7@gmail.com>
2020-06-02 06:21:39 -0700

    Another commit

--- public/js/index.js
+++ public/js/index.js
@@ -2,13 +2,37 @@ var socket = io();
        
 socket.on('connect', function(){
             console.log('connected to the server'); 
-
-            socket.on('replyToClient', function(FromAdmin){
+           
+           /* socket.on('replyToClient', function(FromAdmin){
                 console.log(FromAdmin);
             });
 
             socket.on('excludeUser', function(data){
                 console.log(data);
+            });*/
+
+            socket.on('welcomeMessage', function(welcome_Msg){
+                var li = $('<li></li>');
+                li.text(`${welcome_Msg.admin} : ${welcome_Msg.text}`);
+                $('#feedBack').append(li);
+            });
+
+            $('#message-form').on('submit', function(e){
+                e.preventDefault();
+        
+                socket.emit('createMessage', {
+                    from : 'User',
+                    text : $('[name=message]').val()
+                }, function(passedData){
+                     console.log(passedData);
+                });
+            });
+        
+            socket.on('response', function(feedBack){
+                console.log(feedBack);
+                var li = $('<li></li>');
+                li.text(`${feedBack.from} : ${feedBack.text}`);
+                $('#feedBack').append(li);
             });
 
         });
@@ -17,7 +41,13 @@ socket.on('connect', function(){
            console.log('Disconnect from The server');
        });
 
-       socket.on('newMessage', function(data){
-        console.log(data);
+       socket.on('goodByeMessage', function(bye_Msg){
+        var li = $('<li></li>');
+        li.text(`${bye_Msg.admin} : ${bye_Msg.text}`);
+        $('#feedBack').append(li);
     });
+      
+      /* socket.on('newMessage', function(data){
+        console.log(data);
+    });*/
          
--- server/server.js
+++ server/server.js
@@ -13,30 +13,46 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket)=>{
     console.log('new User Connected');
+
+    socket.broadcast.emit('welcomeMessage', {
+        admin : 'Admin',
+        text : 'New User Joined',
+        time : new Date().getTime()
+    });
+
+    socket.on('createMessage', function(fromClient, callback){
+        console.log(fromClient);
+        socket.broadcast.emit('response', {
+            from : fromClient.from,
+            text : fromClient.text,
+            time : new Date().getTime()
+        }); 
+        callback('message sent');
+    });
    
-    socket.emit('replyToClient',/* {
+    /*socket.emit('replyToClient',/* {
         from : 'Admin',
         text : 'welcome to the chart app',
         time : new Date().getTime()
-    }*/
+    }
      generateMessage('admin', 'Welcome To Chart App')   
     );
 
-    socket.broadcast.emit('excludeUser', /*{
+    socket.broadcast.emit('excludeUser', {
         from : 'Admin',
         text : 'New User Joined',
         time : new Date().getTime()
-    }*/
+    }
     generateMessage('admin', 'new User Joined')
     );
 
    socket.on('createNewEmail', (newEmail)=>{
         console.log(newEmail);
-        io.emit('newMessage',/* {
+        io.emit('newMessage', {
             from : newEmail.from,
             text : newEmail.text,
             createdAT : new Date().getTime()
-        }*/
+        }
         generateMessage(newEmail.from, newEmail.text)
         );
         // socket.broadcast.emit('newMessage',{
@@ -44,10 +60,10 @@ io.on('connection', (socket)=>{
         //     text : newEmail.text,
         //     createdAT : new Date().getTime()
         // });
-   });
+   });*/
 
-    socket.on('disconnect', ()=>{
-        console.log('User was disconnected');
+socket.on('disconnect', ()=>{
+    console.log('User was disconnected');
     });
 });
 
